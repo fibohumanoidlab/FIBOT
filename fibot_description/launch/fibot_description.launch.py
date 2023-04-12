fe8eac2e034d2f069fbbb06dffcb58c4fdc96911
Russapat-Leelawattanakiet <russapat@gmail.com>
2023-04-12 15:08:17 +0700

    fibot description

---
+++ fibot_description/launch/fibot_description.launch.py
@@ -0,0 +1,69 @@
+
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+import os
+
+def generate_launch_description():
+    default_model_path = os.path.join(get_package_share_path('fibot_description'),'fibot001.urdf')
+    default_rviz_config_path = os.path.join(get_package_share_path('fibot_description'),'urdf.rviz') 
+
+    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
+                                    description='Flag to enable joint_state_publisher_gui')
+    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+                                      description='Absolute path to robot urdf file')
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+                                     description='Absolute path to rviz config file')
+
+    # robot_description = ParameterValue(Command(['urdf', LaunchConfiguration('model')]),
+    #                                    value_type=str)
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    with open(default_model_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    robot_state_publisher_node = Node(
+        name='fibot_robot',
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[default_model_path],
+        
+    )
+
+    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
+    joint_state_publisher_node = Node(
+        name='fibot_joint',
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        condition=UnlessCondition(LaunchConfiguration('gui'))
+    )
+
+    # joint_state_publisher_gui_node = Node(
+    #     package='joint_state_publisher_gui',
+    #     executable='joint_state_publisher_gui',
+    #     condition=IfCondition(LaunchConfiguration('gui'))
+    # )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription([
+        gui_arg,
+        model_arg,
+        rviz_arg,
+        joint_state_publisher_node,
+        # joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node
+    ])
